52b64c7c55749c5c42c290f54cbfb68e4efaa7be
Aravind Balla <bsaaravind@gmail.com>
2019-04-08 19:04:21 +0530

    fix: header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,72 +2,58 @@ import React from 'react';
 import { Link } from 'gatsby';
 import { css } from '@emotion/core';
 import theme from '../../config/theme';
-import { StaticQuery, graphql } from 'gatsby';
 
 import Container from './Container';
 
-const Header = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
+const Header = ({ dark, bgColor = 'none', siteTitle, headerColor = 'black' }) => (
+  <header
+    css={css`
+      width: 100%;
+      flex-shrink: 0;
+      background: none;
+      padding: 30px 0 0 0;
+      background: ${dark ? '#090909' : `${bgColor}` || 'none'};
     `}
-    render={({ dark, bgColor = 'none', siteTitle, headerColor = 'black' }) => (
-      <header
+  >
+    <Container noVerticalPadding>
+      <nav
         css={css`
           width: 100%;
-          flex-shrink: 0;
-          background: none;
-          padding: 30px 0 0 0;
-          background: ${dark ? '#090909' : `${bgColor}` || 'none'};
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          color: ${headerColor};
+          a {
+            color: ${headerColor ? headerColor : theme.colors.body_color};
+          }
+          a:hover {
+            color: ${headerColor === theme.colors.white ? 'white' : theme.colors.link_color_hover};
+          }
         `}
       >
-        <Container noVerticalPadding>
-          <nav
-            css={css`
-              width: 100%;
-              display: flex;
-              justify-content: space-between;
-              align-items: center;
-              color: ${headerColor};
-              a {
-                color: ${headerColor ? headerColor : theme.colors.body_color};
+        <Link to="/" aria-label="go to homepage" activeClassName="active">
+          {siteTitle}
+        </Link>
+        <div
+          css={css`
+            font-size: 16px;
+            line-height: 1.25;
+            display: flex;
+            align-items: center;
+            a {
+              color: ${dark ? '#fbfbfb' : 'rgba(0,0,0,0.85)'};
+              text-decoration: none;
+              & + a {
+                margin-left: 32px;
               }
-              a:hover {
-                color: ${headerColor === theme.colors.white
-                  ? 'white'
-                  : theme.colors.link_color_hover};
-              }
-            `}
-          >
-            <Link to="/" aria-label="go to homepage" activeClassName="active">
-              {siteTitle}
-            </Link>
-            <div
-              css={css`
-                font-size: 16px;
-                line-height: 1.25;
-                display: flex;
-                align-items: center;
-                a {
-                  color: ${dark ? '#fbfbfb' : 'rgba(0,0,0,0.85)'};
-                  text-decoration: none;
-                  & + a {
-                    margin-left: 32px;
-                  }
-                }
-                .active {
-                  display: none;
-                  visibility: hidden;
-                }
-              `}
-            >
-              {/*
+            }
+            .active {
+              display: none;
+              visibility: hidden;
+            }
+          `}
+        >
+          {/*
           <Link
             to="/blog"
             activeClassName="active"
@@ -76,12 +62,10 @@ const Header = () => (
             Blog
           </Link>
           */}
-            </div>
-          </nav>
-        </Container>
-      </header>
-    )}
-  />
+        </div>
+      </nav>
+    </Container>
+  </header>
 );
 
 export default Header;
